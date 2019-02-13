dd19b521cf7dc94772c0b8827716aabe5b31bb0c
WalterKahn4 <walter.student.hu.nl>
2019-02-13 13:12:21 +0100

    Wijziging

---
+++ Oefen opdrachten/Practise problem 3.4.py
@@ -0,0 +1,18 @@
+Login = str(input('What is your username?'))
+
+names = ('Joe','Sue','Hani','Sophie')
+
+if Login in names:
+    print('You are in!')
+else:
+    print('unknown')
+print(30 * "_")
+
+Login2 = str(input('What is your username?'))
+
+names2 = ('Joe','Sue','Hani','Sophie')
+
+if Login2 in names2:
+    print('You are in!')
+else:
+    print('unknown')
---
+++ Oefen opdrachten/Practise problem 3.5.py
@@ -0,0 +1,4 @@
+lst = ('stop','desktop','top','post')
+
+for len(i) == 4 in lst:
+    print(i)
