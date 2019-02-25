0089ae81eb3d1a2d1a17584d3666d8bcbbfa06d5
WalterKahn4 <walter.student.hu.nl>
2019-02-25 13:21:24 +0100

    Wijziging

--- Les7/Lesnotes.py
+++ Les7/Lesnotes.py
@@ -1,3 +1,47 @@
 i = 7
 while i <= 37:
     i += 7
+
+def hello():
+    '''a greeting service; it repeatedly requests the name of the user and then greets the user'''
+    while True:
+        name = input('What is your name?')
+        print('hello {}'.format(name))
+
+def cities2():
+    lst = []
+
+    while True:
+        city = input('Enter city: ')
+
+        if city == '':
+            break
+        lst.append(city)
+
+return lst
+
+def before0():
+    for row in table:
+        for num in row:
+            if num == 0:
+                break
+            print(num, end=' ')
+        print()
+
+def sum():
+    total = 0
+    while True:
+        nextInt = input('next int: ')
+        if nextInt == 'quit':
+            break
+        total += int(nextInt)
+    print(total)
+
+employee = []
+employee.append('Yin')
+employee.append('Waad')
+
+employee[0]
+employee[1]
+
+emplyee = {}
